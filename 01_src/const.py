66d50a18599d164e1d6bee1dc2c9ac0c67e2f83e
Mike Halbheer <m.halbheer@terradata.ch>
2022-02-23 11:01:29 +0100

    Initial commit of test data and src

---
+++ 01_src/const.py
@@ -0,0 +1,10 @@
+import os
+
+###############################
+###       DIRECTORIES       ###
+###############################
+
+dir_src = os.path.dirname(__file__)
+dir_root = os.path.dirname(dir_src)
+dir_data = os.path.join(dir_root, '02_data')
+dir_out = os.path.join(dir_root, '03_output')
---
+++ 01_src/main.py
@@ -0,0 +1,7 @@
+import open3d as o3d
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
